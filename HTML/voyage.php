48bf5e21043524d86aaa144060f90684da091012
RayyyZen <rayanmihani@gmail.com>
2025-03-18 15:35:05 +0100

    Création de la page voyage

---
+++ HTML/voyage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Voyage | Star Tour</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="Agence de voyage des lieux de tournage de la saga Star Wars">
+        <meta name="author" content="Rayane M., Enzo F., Hugo N.">
+        <link rel="stylesheet" type="text/css" href="../CSS/style.css">
+        <link rel="stylesheet" type="text/css" href="../CSS/voyage.css">
+    </head>
+
+    <body>
+        <div class="horizontal">
+            <div class="nomSite"> <a href="index.php">Star Tour</a> </div>
+        </div>
+
+        <div class="section pageconnexion">Connexion</div>
+
+        <div class="paragraph pageconnexion">
+            
+        </div>
+    </body>
+</html>
